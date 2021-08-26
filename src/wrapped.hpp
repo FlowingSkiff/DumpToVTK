842b729a3b70223d224a3730116fa05f184ea2b2
FlowingSkiff <boats303@gmail.com>
2021-08-26 00:09:02 -0500

    Fixed warning in wrapping

--- src/wrapped.hpp
+++ src/wrapped.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
-template <typename T>
+template <typename T, 
+    typename = std::enable_if_t<std::is_integral<T>::value>
+>
 struct Wrapped
 {
 public:
     Wrapped(T start, T max):
         m_data(start), m_max(max)
         { }
+    Wrapped() = delete;
     Wrapped& operator++()
     {
-        m_data = ++m_data % m_max;
+        m_data = (m_data + 1) % m_max;
         return *this;
     }
     Wrapped& operator++(int)
@@ -37,6 +40,6 @@ public:
         return m_data;
     }
 private:
-    T m_data;
+    T m_data = 0.0;
     const T m_max;
 };
